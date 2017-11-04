cb92ff97cabe8b00fc446cd207a9248868b20142
Jacquie Soon <Jacquie@192-168-1-101.tpgi.com.au>
2017-11-04 14:14:47 +0800

    hello world 2

--- simple.c
+++ simple.c
@@ -2,8 +2,12 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-        MPI_Init(NULL, NULL);
-        printf("Hello, world!\n");
+        int rank, size;
+
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank); // reports number of processes
+        MPI_Comm_size(MPI_COMM_WORLD, &size); // reports the rank, a number between 0 and size-1 identifying the calling process
+        printf("I am %d of %d\n", rank, size);
         MPI_Finalize();
         return 0;
 }
