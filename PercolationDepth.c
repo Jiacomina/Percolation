370eeb7555422ea7d85a17771d58672e3159159f
Jacquie Soon <Jacquie@192-168-1-101.tpgi.com.au>
2017-11-05 18:08:25 +0800

    -

--- PercolationDepth.c
+++ PercolationDepth.c
@@ -286,7 +286,7 @@ int main(int argc, char* argv[]){
     MPI_Comm_size(MPI_COMM_WORLD, &numProcess); // reports the rank, a number between 0 and size-1 identifying the calling 
 
     float p_seed;
-    srand(time(10));
+    srand(time((time_t*) 1));
     // get start time
     struct timeval start, end;
     gettimeofday(&start, NULL);
