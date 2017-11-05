aa4c037e72e476e8936f008c4727f2f626ba3ae9
Jacquie Soon <Jacquie@192-168-1-101.tpgi.com.au>
2017-11-05 22:02:37 +0800

    bond lattice MPI

--- PercolationDepth.c
+++ PercolationDepth.c
@@ -227,52 +227,54 @@ int checkBond(int r, int c, float p_seed){
     return clusterSize;
 }
 void checkBondLattice(float p_seed){
-    // int largestCluster;
-    // // divide lattice into parts
-    // int num_seg = num_threads;
+    int largestCluster;
+    // divide lattice into parts
+    // int num_seg = numProcess;
     // int sub_lattice_size = LATTICE_SIZE / num_seg;
-    // int clusterSizeArray[num_threads];
-    // for(int i = 0; i < num_threads; i++){
-    //     clusterSizeArray[i] = 0;
-    // }
-    // #pragma omp parallel
-    // {
-    //     // starting from each column of top row
-    //     #pragma omp for
-    //         for(int i = 0; i < num_seg; i++){
-    //             int start = i*sub_lattice_size;
-    //             int end = (i+1)*sub_lattice_size;
-    //             int threadNum = omp_get_thread_num();
-    //             for(int c = start; c < end; c++)
-    //             {
-    //                 int clusterSize = 0;
-    //                 clusterSize = checkBond(0, c, p_seed);
-    //                 if(clusterSize > clusterSizeArray[threadNum]) clusterSizeArray[threadNum] = clusterSize;
-    //             }
-    //         }
-
-    //     #pragma omp for    
-    //         // starting from each row of leftmost column (excluding top left site)
-    //         for(int i = 0; i < num_seg; i++){
-    //             int start = i*sub_lattice_size;
-    //             int end = (i+1)*sub_lattice_size;
-    //             int threadNum = omp_get_thread_num();
-    //             for(int r = start; r < end; r++)
-    //             {
-    //                 int clusterSize = 0;
-    //                 clusterSize = checkBond(r, 0, p_seed);
-    //                 if(clusterSize > clusterSizeArray[threadNum]) clusterSizeArray[threadNum] = clusterSize;
-    //             }
-    //         }
+    
+    // for(int i = 0; i < num_seg; i++){
+    //     int start = i*sub_lattice_size;
+    //     int end = (i+1)*sub_lattice_size;
+    //     for(int c = start; c < end; c++)
+    //     {
+    //         int clusterSize = 0;
+    //         clusterSize = checkBond(0, c, p_seed);
+    //         if(clusterSize > largestCluster) largestCluster = clusterSize;
+    //     }
     // }
 
-    // // get largest cluster from all results from threads
-    // largestCluster = 0;
-    // for(int i = 0; i < num_threads; i++){
-    //     if(clusterSizeArray[i] > largestCluster || largestCluster == 0)
-    //         largestCluster = clusterSizeArray[i];
+    for(int i = 1; i < LATTICE_SIZE; i++){
+        if((i + numProcess) % numProcess == pid){
+            clusterSize = checkBond(0, i, p_seed);
+            if(clusterSize > largestCluster){
+                largestCluster = clusterSize;
+            }
+        }
+    }
+
+    for(int i = 1; i < LATTICE_SIZE; i++){
+        if((i + numProcess) % numProcess == pid){
+            clusterSize = checkBond(i, 0, p_seed);
+            if(clusterSize > largestCluster){
+                largestCluster = clusterSize;
+            }
+        }
+    }
+
+    MPI_Reduce(&largestCluster, &thelargestCluster, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    // // starting from each row of leftmost column (excluding top left site)
+    // for(int i = 0; i < num_seg; i++){
+    //     int start = i*sub_lattice_size;
+    //     int end = (i+1)*sub_lattice_size;
+    //     for(int r = start; r < end; r++)
+    //     {
+    //         int clusterSize = 0;
+    //         clusterSize = checkBond(r, 0, p_seed);
+    //         if(clusterSize > largestCluster[threadNum]) largestCluster = clusterSize;
+    //     }
     // }
-    // printf("--------------------------\n");
+
 }
 
 int main(int argc, char* argv[]){
@@ -370,7 +372,8 @@ int main(int argc, char* argv[]){
                     break;
                 default: fprintf(stderr, "Percolation Type Invalid\n");
             }
-                printf("Largest Cluster: %i", thelargestCluster);
+                MPI_Barrier(MPI_COMM_WORLD);
+                printf("Largest Cluster: %i\n", thelargestCluster);
         }
     }
     else {
